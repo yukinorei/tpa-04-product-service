ae157b850696407c0d6adcecbe227e4761e15dc6
Ryosuke Kawamura <clonable.eden@gmail.com>
2018-10-08 19:56:03 +0900

    fix index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-const initProductService = require('./server/product-service');
+const setup = require('./server/setup');
 
 app.use(express.static('public'));
 app.set('port', process.env.PORT || 3000);
 
-initProductService(app);
+setup(app);
 
 const server = http.createServer(app);
 server.listen(app.get('port'), () => {
   console.log(`Server listening on port ${app.get('port')}...`);
 });
-
