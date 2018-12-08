7e8f4fcb2dfb052071c05346e348446abbb14ccd
yukinorei <>
2018-12-08 22:14:26 +0900

    priceのハンドリング追加

--- server/controller.js
+++ server/controller.js
@@ -1,10 +1,23 @@
-const { getProducts } = require('./product.js');
+const { getProducts, getTotalPrice } = require('./product.js');
 
 const getProductsObj = (req, res) => {
   const productObj = { products: getProducts() };
   res.json(productObj);
 };
 
+const getTotalPriceHandler = (req, res) => {
+  console.log(req, res);
+  const getIds = req.query.ids;
+  const productIds = getIds.split(',');
+  const total = getTotalPrice(productIds);
+  const productObj = {
+    productIds,
+    total,
+  };
+  res.json(productObj);
+};
+
 module.exports = {
   getProductsObj,
+  getTotalPriceHandler,
 };
--- server/setup.js
+++ server/setup.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getProductsObj } = require('./controller.js');
+const { getProductsObj, getTotalPriceHandler } = require('./controller.js');
 
 const setup = (app) => {
   const apiRouter = express.Router();
   apiRouter.get('/products', getProductsObj);
+  apiRouter.get('/price', getTotalPriceHandler);
   app.use('/api', apiRouter);
 };
 
